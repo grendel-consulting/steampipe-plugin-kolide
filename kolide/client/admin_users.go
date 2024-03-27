6164db96bcde2fc0286093684e7d3350b3fc4bd6
James Ramirez <james.ramirez@grendel-consulting.com>
2024-03-27 21:33:20 +0000

    refactor: use new helper methods

--- kolide/client/admin_users.go
+++ kolide/client/admin_users.go
@@ -2,11 +2,13 @@ package kolide_client
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 )
 
 type AdminUserListResponse struct {
 	AdminUsers []AdminUser `json:"data"`
+	Pagination Pagination  `json:"pagination"`
 }
 type AdminUser struct {
 	Id        string    `json:"id"`
@@ -17,17 +19,45 @@ type AdminUser struct {
 	Access    string    `json:"access"`
 }
 
-func (c *Client) GetAdminUsers() (*AdminUserListResponse, error) {
-	res, err := c.r().Get("/admin_users/")
+func (c *Client) GetAdminUsers(cursor string, limit int32, searches ...Search) (interface{}, error) {
+	params := make(map[string]string)
+	params["query"] = serializeSearches(searches)
+
+	if cursor != "" {
+		params["per_page"] = strconv.Itoa(int(limit))
+		params["cursor"] = cursor
+	}
+
+	res, err := c.r().SetQueryParams(params).Get("/admin_users/")
 
 	if err != nil {
-		return nil, fmt.Errorf("error retrieving AdminUsers: %q", err)
+		return nil, fmt.Errorf("error retrieving deprovisioned people: %q", err)
 	}
 
 	if !res.IsSuccessState() {
-		return nil, fmt.Errorf("error retrieving AdminUsers: %q", res.Status)
+		return nil, fmt.Errorf("error retrieving deprovisioned people: %q", res.Status)
 	}
 	var response AdminUserListResponse
+
+	err = res.UnmarshalJson(&response)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling JSON: %q", err)
+	}
+
+	return &response, nil
+}
+
+func (c *Client) GetAdminUserById(id string) (*AdminUser, error) {
+	res, err := c.r().SetPathParam("adminUserId", id).Get("/admin_users/{adminUserId}")
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving admin users: %q", err)
+	}
+
+	if !res.IsSuccessState() {
+		return nil, fmt.Errorf("error retrieving admin user: %q", res.Status)
+	}
+	var response AdminUser
 
 	err = res.UnmarshalJson(&response)
 	if err != nil {
--- kolide/table_kolide_k2_admin_user.go
+++ kolide/table_kolide_k2_admin_user.go
@@ -3,6 +3,7 @@ package kolide
 import (
 	"context"
 
+	kolide "github.com/grendel-consulting/steampipe-plugin-kolide/kolide/client"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
@@ -15,33 +16,31 @@ func tableKolideK2AdminUser(_ context.Context) *plugin.Table {
 		Name:        "kolide_k2_admin_user",
 		Description: "Users with access to the Kolide dashboard.",
 		Columns: []*plugin.Column{
+			// Filterable "top" columns
 			{Name: "id", Description: "Canonical identifier for this admin user.", Type: proto.ColumnType_STRING},
 			{Name: "first_name", Description: "First name for this admin user.", Type: proto.ColumnType_STRING},
 			{Name: "last_name", Description: "Last human name for this admin user.", Type: proto.ColumnType_STRING},
 			{Name: "email", Description: "Email address for this admin user.", Type: proto.ColumnType_STRING},
 			{Name: "created_at", Description: "When this admin user account was created.", Type: proto.ColumnType_TIMESTAMP},
+			// Other columns
 			{Name: "access", Description: "Access level granted to this admin user, one of full, limited or billing.", Type: proto.ColumnType_STRING},
 			// Restrictions {}
 			// Steampipe standard columns
-			{
-				Name: "title", Description: "Display name for this admin user.", Type: proto.ColumnType_STRING,
-				Transform: transform.FromField("email"),
-			},
-			// {
-			// 	Name:        "akas",
-			// 	Description: "Array of also-known-as identifiers that uniquely identify this resource.",
-			// 	Type:        proto.ColumnType_JSON,
-			// 	Transform:   ...
-			// },
-			// {
-			// 	Name:        "tags",
-			// 	Description: "Any tags or labels on this resource.",
-			// 	Type:        proto.ColumnType_JSON,
-			// 	Transform:   ...
-			// },
+			{Name: "title", Description: "Display name for this admin user.", Type: proto.ColumnType_STRING, Transform: transform.FromField("Name")},
 		},
 		List: &plugin.ListConfig{
+			KeyColumns: []*plugin.KeyColumn{
+				// Using Kolide K2 API query feature, can be combined with AND (and OR)
+				{Name: "first_name", Require: plugin.Optional, Operators: []string{"=", "~~"}},
+				{Name: "last_name", Require: plugin.Optional, Operators: []string{"=", "~~"}},
+				{Name: "email", Require: plugin.Optional, Operators: []string{"=", "~~"}},
+				{Name: "created_at", Require: plugin.Optional, Operators: []string{"=", ">", "<"}},
+			},
 			Hydrate: listAdminUsers,
+		},
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("id"),
+			Hydrate:    getDevice,
 		},
 	}
 }
@@ -49,19 +48,19 @@ func tableKolideK2AdminUser(_ context.Context) *plugin.Table {
 //// LIST FUNCTION
 
 func listAdminUsers(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	client, err := connect(ctx, d)
+	var visitor ListPredicate = func(client *kolide.Client, cursor string, limit int32, searches ...kolide.Search) (interface{}, error) {
+		return client.GetAdminUsers(cursor, limit, searches...)
+	}
 
-	if err != nil {
-		plugin.Logger(ctx).Error("kolide_k2_admin_user.listAdminUsers", "connection_error", err)
-		return nil, err
+	return listAnything(ctx, d, h, "kolide_k2_admin_user.listAdminUsers", visitor, "AdminUsers")
+}
+
+//// GET FUNCTION
+
+func getAdminUser(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	var visitor GetPredicate = func(client *kolide.Client, id string) (interface{}, error) {
+		return client.GetAdminUserById(id)
 	}
-	res, err := client.GetAdminUsers()
-	if err != nil {
-		plugin.Logger(ctx).Error("kolide_k2_admin_user.listAdminUsers", err)
-		return nil, err
-	}
-	for _, user := range res.AdminUsers {
-		d.StreamListItem(ctx, user)
-	}
-	return nil, nil
+
+	return getAnything(ctx, d, h, "kolide_k2_admin_user.getAdminUser", "id", visitor)
 }
